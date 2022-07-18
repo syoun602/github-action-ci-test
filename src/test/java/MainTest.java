37e2d7b9a791583607f7a39ddc0c0295a76c28b0
syoun602 <syoun602@gmail.com>
2022-07-18 17:12:14 +0900

    commit to dev

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void some() {
+        final boolean bool = true;
+        assertTrue(bool);
+    }
+}
